1cfae7ec0dc246cfd9b585a9cc97ab7997bdc32f
RubsRz <rodriguezperezrubenalejandro@gmail.com>
2022-02-16 21:51:51 -0600

    complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,95 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  nombre: string;
+  telefono: number;
+  email: string;
+  notas: string;
+  contactos: any[];
+  toggle: boolean;
+
+  constructor(public alerta: AlertController) {
+    this.nombre = '';
+    // this.telefono = 0;
+    this.email = '';
+    this.notas = '';
+    this.contactos = [];
+    this.toggle = false;
+  }
+
+
+  addContacto(){
+    // eslint-disable-next-line eqeqeq
+    if (this.nombre == '') {
+      // alert('Escribe los campos correctamente');
+      this.errorAlert();
+    }
+    else{
+      const nuevoContacto = {
+        name: this.nombre,
+        tel: this.telefono,
+        correo: this.email,
+        note: this.notas
+      };
+
+      this.contactos.push(nuevoContacto);
+      // console.log(this.fruitList);
+      this.nombre = '';
+      this.telefono = 0;
+      this.email = '';
+      this.notas = '';
+      console.log(this.contactos);
+      this.basicAlert();
+    }
+  }
+
+  deleteCont(indiceCont){
+    this.contactos.splice(indiceCont, 1);
+  }
+
+  async basicAlert() {
+    const alert = await this.alerta.create({
+      header: 'ÉXITO',
+      subHeader: 'Contacto añadido',
+      // message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async errorAlert() {
+    const alert = await this.alerta.create({
+      header: 'ALGO SALIÓ MAL',
+      subHeader: 'Llena los campos correctamente',
+      // message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async alertConfirm(indiceCont) {
+    const alert = await this.alerta.create({
+      header: 'ELIMINAR CONTACTO!',
+      message: 'Por favor confirma que quieres eliminar a ' + this.contactos[indiceCont].name + ' de tus contactos',
+      buttons: [
+        {
+          text: 'Cancel',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Confirmar',
+          id: 'confirm-button',
+          handler: (blah) => {
+            console.log('Confirm Okay');
+            this.contactos.splice(indiceCont, 1);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
